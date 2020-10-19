e3af3a17600d1a448d72c32d4361b9a1d7c73ac2
VivekDSrivastava <developer.viveksrivastava@gmail.com>
2020-10-20 00:01:29 +0530

    updated automated script

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# ridecell-code
+# Parking Managment Service
+
+
+We are building a street parking spot reservation service. Each parking spot is identified by its location (lat, lng). Users should be able to view street parking spots, reserve and pay for the parking spots or cancel their reservations.
+
+This Repo serves as a REST API.
+
+**Tech Stack**
+1. Python 3.8
+2. Django 3.1
+3. Postgres 10 & Postgis
+4. Django Rest Framework
+
+**Current Features:**
+
+ 1. See available parking spots on a map
+ 2. Search for an address and find nearby parking spot. (input: lat, lng, radius in meters. Output -
+    list of parking spots within the radius).
+ 3. Reserve a parking spot
+ 4. View existing reservations
+ 5. Cancel an existing reservation
+ 6. Show the user the cost of the reservation
+ 7. Unit tests available
+ 8. Automated tests  Available
+ 9. Require users to use phone numbers to sign up.
+ 10. Validate that the phone numbers are real.
--- runner.py
+++ runner.py
@@ -1,36 +1,64 @@
+import datetime
+
 import requests
 from requests.auth import HTTPBasicAuth
 
 USERNAME = '8657946755'
 PASSWORD = 'Qwerty@1234'
 
-def display_names(l):
-    for spot in l:
-        print(spot['code'])
 
-def perform_signup():
+
+def perform_sign_up():
     url = 'http://127.0.0.1:8001/api/v1/users/signup/'
     user_data = {'contact_number': USERNAME, 'password': PASSWORD}
-    x = requests.post(url, data=user_data)
-    print(x.text)
+    print(f'CREATING A NEW USER WITH USERNAME {USERNAME} AND PASSWORD {PASSWORD}')
+    create_user_response = requests.post(url, data=user_data)
+    print(create_user_response.json())
 
 def get_parking_spots():
     url = 'http://127.0.0.1:8001/api/v1/parkings/'
-    x = requests.get(url,  auth=HTTPBasicAuth(USERNAME, PASSWORD))
-    print('{0} results on earth'.format(len(x.json())))
-    print(display_names(x.json()))
+    print(f'GETING ALL PARKING SPOTS ON EARTH WITHOUT FILTER')
+    parking_spots = requests.get(url,  auth=HTTPBasicAuth(USERNAME, PASSWORD))
+    print('{0} parking spots found on earth'.format(len(parking_spots.json())))
 
 
 def get_parking_spots_in_radius(radius=500):
     url = 'http://127.0.0.1:8001/api/v1/parkings/?lat=18.567124&long=73.769539&radius={0}'.format(
         radius)
-    x = requests.get(url, auth=HTTPBasicAuth(USERNAME, PASSWORD))
-    print('{1} results in {0} m radius'.format(radius, len(x.json())))
-    print(display_names(x.json()))
+    print(f'GETING ALL PARKING SPOTS IN RADIUS OF {radius}')
+    parking_spots = requests.get(url, auth=HTTPBasicAuth(USERNAME, PASSWORD))
+    print('{1} results in {0} m radius'.format(radius, len(parking_spots.json())))
 
 
-perform_signup()
+def book_parking_spot():
+    url = 'http://127.0.0.1:8001/api/v1/parkings/'
+    x = requests.get(url,  auth=HTTPBasicAuth(USERNAME, PASSWORD))
+    print('Performing Reservation for {0}'.format(x.json()[0]['code']))
+    start_time = datetime.datetime.now()
+    end_time = datetime.datetime.now()+datetime.timedelta(minutes=10)
+    payload = {
+            'from_time': start_time,
+            'valid_up_to': end_time,
+    }
+    booking_response = requests.post('{}{}/'.format(x.json()[0]['url'],'reserve'),
+                                      data=payload,auth=HTTPBasicAuth(USERNAME, PASSWORD))
+    print('Reservation Done  for {0} with response {1}'.format(x.json()[0]['code'],booking_response))
+
+
+def cancel_parking_spot_booking():
+    url = 'http://127.0.0.1:8001/api/v1/bookings/'
+    bookings = requests.get(url,  auth=HTTPBasicAuth(USERNAME, PASSWORD))
+    for booking in bookings.json():
+        x = requests.get('{}{}/'.format(booking['url'],'cancel'), auth=HTTPBasicAuth(USERNAME, PASSWORD))
+        print(x.json())
+
+
+perform_sign_up()
 get_parking_spots()
 get_parking_spots_in_radius()
 get_parking_spots_in_radius(1000)
 get_parking_spots_in_radius(50000)
+book_parking_spot()
+get_parking_spots()
+cancel_parking_spot_booking()
+get_parking_spots()
